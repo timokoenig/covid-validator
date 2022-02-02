01c5aaa8872e783cbcb2e19979ad3686d874008a
Timo Koenig <timo@timokoenig.com>
2022-02-02 18:34:23 +0100

    Add qr code scanner

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
-import { Text } from '@chakra-ui/react';
+import { Container, Heading } from '@chakra-ui/react';
+import dynamic from 'next/dynamic';
+
+const BarcodeScannerComponent = dynamic(() => import('react-qr-barcode-scanner'), {
+  ssr: false,
+});
 
 const IndexPage = () => {
-  return <Text>Hello World</Text>;
+  const [data, setData] = React.useState<string>('');
+
+  return (
+    <Container marginTop={10}>
+      <Heading as="h1" size="2xl">
+        Covid Check
+      </Heading>
+      <BarcodeScannerComponent
+        width={500}
+        height={500}
+        onUpdate={(_, result) => {
+          if (result) {
+            setData(result.getText());
+          }
+        }}
+      />
+      <p>{data}</p>
+    </Container>
+  );
 };
 
 export default IndexPage;
