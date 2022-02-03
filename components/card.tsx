7363a31e517bbfb754130e58daf8df4330994dd4
Timo Koenig <timo@timokoenig.com>
2022-02-03 01:06:34 +0100

    Add camera view to main card

--- components/card.tsx
+++ components/card.tsx
@@ -1,31 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Box, Text, Center } from '@chakra-ui/react'
 import CardHeader from './card-header'
+import dynamic from 'next/dynamic'
+
+const BarcodeScannerComponent = dynamic(() => import('react-qr-barcode-scanner'), {
+  ssr: false,
+})
 
 const Card = () => {
+  const [scanMode, setScanMode] = useState<boolean>(false)
   return (
     <Box borderRadius="lg" overflow="hidden" bg="blue.400" boxShadow="xl" mb="10">
       <CardHeader />
 
-      <Box p="6">
-        <Center py="10">
-          <Button
-            size="lg"
-            variant="outline"
-            color="white"
-            backgroundColor={'blue.400'}
-            _hover={{ bg: 'blue.300' }}
-            _active={{ bg: 'blue.400' }}
-          >
-            Check Certificate
-          </Button>
-        </Center>
-        <Center pb="10">
-          <Text fontWeight="semibold" color="white">
-            some more information for the user
-          </Text>
-        </Center>
-      </Box>
+      {scanMode ? (
+        <>
+          <BarcodeScannerComponent
+            onUpdate={() => {}}
+            // onUpdate={(_, result) => {
+            //   if (!result || result.getText() == data) return
+            //   setData(result.getText())
+            // }}
+          />
+          {/* <p>{data}</p> */}
+          <Center py="5">
+            <Text color="white">Hold the QR Code in front of your camera</Text>
+          </Center>
+          <Center pb="5">
+            <Button
+              size="lg"
+              variant="outline"
+              color="white"
+              backgroundColor={'blue.400'}
+              _hover={{ bg: 'blue.300' }}
+              _active={{ bg: 'blue.400' }}
+              onClick={() => setScanMode(false)}
+            >
+              Close Camera
+            </Button>
+          </Center>
+        </>
+      ) : (
+        <Box p="6">
+          <Center py="10">
+            <Button
+              size="lg"
+              variant="outline"
+              color="white"
+              backgroundColor={'blue.400'}
+              _hover={{ bg: 'blue.300' }}
+              _active={{ bg: 'blue.400' }}
+              onClick={() => setScanMode(true)}
+            >
+              Check Certificate
+            </Button>
+          </Center>
+          <Center pb="10">
+            <Text fontWeight="semibold" color="white">
+              some more information for the user
+            </Text>
+          </Center>
+        </Box>
+      )}
     </Box>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,11 @@
 import React from 'react'
 import { Container } from '@chakra-ui/react'
-// import dynamic from 'next/dynamic'
 // import { decodeOnly, DCCData } from 'dcc-decoder'
 // import dccConfig from '../utils/dccConfig'
 import Header from '../components/header'
 import Card from '../components/card'
 import Information from '../components/information'
 import Footer from '../components/footer'
-
-// const BarcodeScannerComponent = dynamic(() => import('react-qr-barcode-scanner'), {
-//   ssr: false,
-// })
 
 const IndexPage = () => {
   // const [data, setData] = React.useState<string>('')
@@ -43,15 +38,6 @@ const IndexPage = () => {
       <Header />
       <Card />
       <Information />
-      {/* <BarcodeScannerComponent
-        width={500}
-        height={500}
-        onUpdate={(_, result) => {
-          if (!result || result.getText() == data) return
-          setData(result.getText())
-        }}
-      />
-      <p>{data}</p> */}
       <Footer />
     </Container>
   )
