86949099e990c76bf011d7ec066d1eeb2e95584e
Timo Koenig <timo@timokoenig.com>
2022-02-03 23:10:38 +0100

    Add some example information

--- components/information.tsx
+++ components/information.tsx
@@ -5,15 +5,31 @@ const Information = () => (
   <Box mb="10">
     <Box mb="5">
       <Heading as="h4" size="md">
-        What is this?
+        What is the purpose of Covid Check?
       </Heading>
-      <Text>some more information about this project</Text>
+      <Text>to look good</Text>
     </Box>
     <Box mb="5">
       <Heading as="h4" size="md">
-        What is this?
+        What does{' '}
+        <Text display="inline" color="red">
+          BETA
+        </Text>{' '}
+        mean?
       </Heading>
-      <Text>some more information about this project</Text>
+      <Text>that it will most likely crash at some point</Text>
+    </Box>
+    <Box mb="5">
+      <Heading as="h4" size="md">
+        Where do the the rules come from?
+      </Heading>
+      <Text>from the cloud</Text>
+    </Box>
+    <Box mb="5">
+      <Heading as="h4" size="md">
+        Where do I get help if something is not working?
+      </Heading>
+      <Text>visit github and create an issue</Text>
     </Box>
   </Box>
 )
