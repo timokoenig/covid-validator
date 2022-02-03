78b47ecb724a40ec4066e45b46d1fae5962f1502
Timo Koenig <timo@timokoenig.com>
2022-02-03 22:42:55 +0100

    Add basic layout for success and failed result modal

--- components/result-modal.tsx
+++ components/result-modal.tsx
@@ -3,12 +3,13 @@ import {
   Modal,
   ModalOverlay,
   ModalContent,
-  ModalHeader,
   ModalFooter,
   ModalBody,
-  ModalCloseButton,
   Button,
   Text,
+  Center,
+  Box,
+  Heading,
 } from '@chakra-ui/react'
 import { VerificationResult } from 'dcc-decoder'
 
@@ -19,25 +20,98 @@ type Props = {
 }
 
 const ResultModal = (props: Props) => {
+  const success = true
+
+  const CertValid = () => (
+    <ModalContent overflow="hidden" bg="green.400">
+      {/* <ModalCloseButton color="white" /> */}
+      <ModalBody mb="5">
+        <Center p="10">
+          <Heading color="white">VALID</Heading>
+        </Center>
+        <Center display="flex" flexDirection="row">
+          <Box px="5">
+            <Text fontWeight="semibold" fontSize="xl" color="white">
+              Name
+            </Text>
+            <Text fontWeight="semibold" fontSize="xl" color="white">
+              Birthdate
+            </Text>
+          </Box>
+          <Box px="5">
+            <Text fontSize="xl" color="white">
+              Timo Koenig
+            </Text>
+            <Text fontSize="xl" color="white">
+              01.01.2000
+            </Text>
+          </Box>
+        </Center>
+      </ModalBody>
+
+      <ModalFooter>
+        <Button
+          size="lg"
+          variant="outline"
+          color="white"
+          backgroundColor={'green.400'}
+          _hover={{ bg: 'green.300' }}
+          _active={{ bg: 'green.400' }}
+          onClick={props.onClose}
+        >
+          Close
+        </Button>
+      </ModalFooter>
+    </ModalContent>
+  )
+
+  const CertInvalid = () => (
+    <ModalContent overflow="hidden" bg="red.400">
+      {/* <ModalCloseButton color="white" /> */}
+      <ModalBody mb="5">
+        <Center p="10">
+          <Heading color="white">NOT VALID</Heading>
+        </Center>
+        <Center display="flex" flexDirection="row">
+          <Box px="5">
+            <Text fontWeight="semibold" fontSize="xl" color="white">
+              Name
+            </Text>
+            <Text fontWeight="semibold" fontSize="xl" color="white">
+              Birthdate
+            </Text>
+          </Box>
+          <Box px="5">
+            <Text fontSize="xl" color="white">
+              Timo Koenig
+            </Text>
+            <Text fontSize="xl" color="white">
+              01.01.2000
+            </Text>
+          </Box>
+        </Center>
+      </ModalBody>
+
+      <ModalFooter>
+        <Button
+          size="lg"
+          variant="outline"
+          color="white"
+          backgroundColor={'red.400'}
+          _hover={{ bg: 'red.300' }}
+          _active={{ bg: 'red.400' }}
+          onClick={props.onClose}
+        >
+          Close
+        </Button>
+      </ModalFooter>
+    </ModalContent>
+  )
+
   return (
     <Modal isOpen={props.isOpen} onClose={props.onClose} size="lg">
       <ModalOverlay />
-      <ModalContent>
-        <ModalHeader>Result Modal</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody>
-          {props.result?.error && <Text>{props.result?.error.name}</Text>}
-          {/* {props.result?.ruleErrors && <Text>{props.result?.ruleErrors}</Text>} */}
-          <Text>{props.result?.cert?.nam.gn + '' + props.result?.cert?.nam.fn}</Text>
-        </ModalBody>
-
-        <ModalFooter>
-          <Button colorScheme="blue" mr={3} onClick={props.onClose}>
-            Close
-          </Button>
-          <Button variant="ghost">Secondary Action</Button>
-        </ModalFooter>
-      </ModalContent>
+      {success ? <CertValid /> : <CertInvalid />}
     </Modal>
   )
 }
