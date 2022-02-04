65ac34e2b74e3c10daf56b50d5dc76bda004d29b
Timo Koenig <timo@timokoenig.com>
2022-02-04 02:34:28 +0100

    Add some boilerplate for the cert logic

---
+++ utils/certlogic.ts
@@ -0,0 +1,48 @@
+import { DCC } from './dcc'
+import countryBusinessRules from './eu-dcc-rules.json'
+
+type Rules = {
+  updatedAt: string
+  rules: Rule[]
+}
+
+type Language = {
+  lang: string
+  desc: string
+}
+
+type Rule = {
+  Identifier: string
+  Hash?: string | null
+  Type: string
+  Country: string
+  Version: string
+  SchemaVersion: string
+  Engine: string
+  EngineVersion: string
+  CertificateType: string
+  Description: Language[]
+  ValidFrom: string
+  ValidTo: string
+  AffectedFields: string[]
+  Logic: any
+}
+
+export type DCCRuleValidationResult = {
+  rules: Rule[]
+  failedRules: Rule[]
+  valid: boolean
+}
+
+export async function validateDCCRules(
+  dcc: DCC,
+  country: string,
+  validationClock: Date
+): Promise<DCCRuleValidationResult> {
+  const allRules = countryBusinessRules as Rules
+  return {
+    rules: [],
+    failedRules: [],
+    valid: false,
+  }
+}
