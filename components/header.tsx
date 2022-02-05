6184c0a4360df0e9187dd9e25e78249a045401bb
Timo Koenig <timo@timokoenig.com>
2022-02-05 03:13:43 +0100

    Add subtitle below logo

--- components/header.tsx
+++ components/header.tsx
@@ -20,30 +20,33 @@ type Props = {
 const Header = (props: Props) => {
   const { toggleColorMode, newColorMode } = useColorMode()
   return (
-    <Box display="flex">
-      <Heading as="h1" size="2xl" marginBottom={5} flex="1">
-        <Link href="/" _hover={{ textDecoration: 'none' }}>
-          Covid Check
-        </Link>{' '}
-        <Text display="inline" fontSize="lg" color="red">
-          BETA
-        </Text>
-      </Heading>
-      {(props.showMenu ?? true) && (
-        <Menu>
-          <MenuButton
-            as={IconButton}
-            aria-label="Options"
-            icon={<HamburgerIcon />}
-            variant="outline"
-          />
-          <MenuList>
-            <MenuItem onClick={toggleColorMode}>
-              Toggle {newColorMode === 'light' ? 'Dark' : 'Light'}
-            </MenuItem>
-          </MenuList>
-        </Menu>
-      )}
+    <Box mb="5">
+      <Box display="flex">
+        <Heading as="h1" size="2xl" flex="1">
+          <Link href="/" _hover={{ textDecoration: 'none' }}>
+            Covid Check
+          </Link>{' '}
+          <Text display="inline" fontSize="lg" color="red">
+            BETA
+          </Text>
+        </Heading>
+        {(props.showMenu ?? true) && (
+          <Menu>
+            <MenuButton
+              as={IconButton}
+              aria-label="Options"
+              icon={<HamburgerIcon />}
+              variant="outline"
+            />
+            <MenuList>
+              <MenuItem onClick={toggleColorMode}>
+                Toggle {newColorMode === 'light' ? 'Dark' : 'Light'}
+              </MenuItem>
+            </MenuList>
+          </Menu>
+        )}
+      </Box>
+      <Text fontWeight="semibold">Validator for the EU Digital Covid Certificate</Text>
     </Box>
   )
 }
