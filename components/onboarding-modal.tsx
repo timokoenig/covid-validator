711811cd28aa00cd3b4eb74c5b0c8e6e4b095c2c
Timo Koenig <timo@timokoenig.com>
2022-02-06 16:36:49 +0100

    Update onboarding text

--- components/onboarding-modal.tsx
+++ components/onboarding-modal.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   Modal,
   ModalOverlay,
@@ -8,6 +8,12 @@ import {
   ModalBody,
   Button,
   Text,
+  Checkbox,
+  Link,
+  ListItem,
+  Heading,
+  UnorderedList,
+  Box,
 } from '@chakra-ui/react'
 
 type Props = {
@@ -16,6 +22,7 @@ type Props = {
 }
 
 const OnboardingModal = (props: Props) => {
+  const [accepted, setAccepted] = useState<boolean>(false)
   const onSave = () => {
     localStorage.setItem('onboarding', 'true')
     props.onClose()
@@ -27,12 +34,59 @@ const OnboardingModal = (props: Props) => {
       <ModalContent>
         <ModalHeader>Covid Validator</ModalHeader>
         <ModalBody>
-          <Text>Placeholder text for onboarding</Text>
+          <Text>
+            Covid Validator allows you to scan and validate <b>EU Digital Covid Certificates </b>
+            based on country or local rules. The scan and validation process happens locally in your
+            browser.
+          </Text>
+          <Heading size="sm" mt="5">
+            The website
+          </Heading>
+          <UnorderedList>
+            <ListItem>does not save any personal information</ListItem>
+            <ListItem>does not share any information with others</ListItem>
+            <ListItem>
+              does not use any cookies (required data is saved in your browsers local storage)
+            </ListItem>
+            <ListItem>
+              is Open Source and can be found on
+              <Link
+                href="http://github.com/timokoenig/covid-validator"
+                target="_blank"
+                style={{ textDecoration: 'underline' }}
+                mx="1"
+              >
+                Github
+              </Link>
+            </ListItem>
+            <ListItem>
+              is hosted in Germany by
+              <Link
+                href="https://www.hetzner.com/"
+                target="_blank"
+                style={{ textDecoration: 'underline' }}
+                mx="1"
+              >
+                Hetzner Online GmbH
+              </Link>
+            </ListItem>
+          </UnorderedList>
+
+          <Box my="5">
+            <Link href="/privacy" target="_blank" style={{ textDecoration: 'underline' }}>
+              Read more about the Privacy Policy
+            </Link>
+          </Box>
+
+          <Button isFullWidth onClick={() => setAccepted(!accepted)}>
+            <Checkbox isChecked={accepted} mr="5" />
+            <Text>I agree to the Privacy Policy</Text>
+          </Button>
         </ModalBody>
 
         <ModalFooter>
-          <Button colorScheme="blue" mr={3} onClick={onSave}>
-            Accept
+          <Button colorScheme="blue" mr={3} onClick={onSave} isDisabled={!accepted}>
+            Continue
           </Button>
         </ModalFooter>
       </ModalContent>
