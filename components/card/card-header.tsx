db0e74b818a03ca9f55b03f6658831393d45a2bc
Moritz Veltrup <moritz.veltrup@gmail.com>
2022-02-10 16:48:57 +0100

    Update card header

--- components/card/card-header.tsx
+++ components/card/card-header.tsx
@@ -1,18 +1,18 @@
-import React, { useState } from 'react'
-import { Button, Text, Box, Spacer, useDisclosure } from '@chakra-ui/react'
 import { ChevronDownIcon } from '@chakra-ui/icons'
+import { Box, Button, Text, useDisclosure } from '@chakra-ui/react'
+import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import purpose from '../../utils/purpose'
 import countries from '../../utils/countries'
-import RuleModal from '../modal/rule'
-import PurposeModal from '../modal/purpose'
+import purpose from '../../utils/purpose'
 import Flag from '../flag'
+import PurposeModal from '../modal/purpose'
+import RuleModal from '../modal/rule'
 
 const CardHeader = () => {
   const { t } = useTranslation('country')
   const { t: tCommon } = useTranslation('common')
   const { isOpen: isOpenRule, onOpen: onOpenRule, onClose: onCloseRule } = useDisclosure()
-  const { isOpen: isOpenPurpose, onOpen: onOpenPurpose, onClose: onClosePurpose } = useDisclosure()
+  const { isOpen: isOpenPurpose, onClose: onClosePurpose } = useDisclosure()
   const [selection, setSelection] = useState<{ country: string; state: string }>({
     country: localStorage.getItem('country') ?? 'DE',
     state: localStorage.getItem('state') ?? '',
@@ -29,45 +29,40 @@ const CardHeader = () => {
     <>
       <Box display="flex" flexDirection="row">
         <Button
-          px="5"
-          py="10"
-          isFullWidth
+          px="6"
+          pt="5"
+          pb="1"
+          h="auto"
           backgroundColor="blue.300"
           _hover={{ bg: 'blue.200' }}
           _active={{ bg: 'blue.300' }}
+          color="white"
           display="flex"
           flexDirection="row"
+          flexWrap="wrap"
+          flexGrow="1"
           alignItems="center"
+          justifyItems="stretch"
+          justifyContent="space-between"
           onClick={onOpenRule}
           borderRadius="0"
         >
-          <Flag country={country.code.toLowerCase()} size={25} />
-          <Text fontSize="h4" fontWeight="bold" color="white" ml="2">
-            {country.name} / {state.name}
-          </Text>
-          <ChevronDownIcon w={8} h={8} color="white" />
-          <Spacer />
-        </Button>
-        {selection.country == 'DE' && (
-          <Button
-            pl="10"
-            pr="5"
-            py="10"
-            backgroundColor="blue.300"
-            _hover={{ bg: 'blue.200' }}
-            _active={{ bg: 'blue.300' }}
-            display="flex"
-            flexDirection="row"
-            alignItems="center"
-            onClick={onOpenPurpose}
-            borderRadius="0"
-          >
-            <Text fontSize="h4" fontWeight="bold" color="white">
-              {selectionPurpose}
+          <Box>
+            <Flag country={country.code.toLowerCase()} size={25} />
+          </Box>
+          <Box textAlign="center" flex="1" mx="6">
+            <Text fontSize="lg" wordBreak="break-word" whiteSpace="normal" fontWeight="bold">
+              {country.name}
             </Text>
-            <ChevronDownIcon w={8} h={8} color="white" />
-          </Button>
-        )}
+            <Text fontSize="md" wordBreak="break-word" whiteSpace="normal" fontWeight="400">
+              {state.name}
+            </Text>
+          </Box>
+          <Text fontSize="lg" justifySelf="flex-end" fontWeight="bold" color="white">
+            {selectionPurpose}
+          </Text>
+          <ChevronDownIcon mt="1" flex="1 0 100%" w={6} h={6} color="white" />
+        </Button>
       </Box>
       <RuleModal isOpen={isOpenRule} onClose={onCloseRule} onChange={setSelection} />
       <PurposeModal
