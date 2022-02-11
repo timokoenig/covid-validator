db7d28d9f3c4a1c5c02ec6c5122a285ba251740e
Timo Koenig <timo@timokoenig.com>
2022-02-11 18:20:59 +0100

    Add toast to indicate that rules are saved successfully

--- pages/builder.tsx
+++ pages/builder.tsx
@@ -1,4 +1,4 @@
-import { Flex, useDisclosure } from '@chakra-ui/react'
+import { Flex, useDisclosure, useToast } from '@chakra-ui/react'
 import React, { useState } from 'react'
 import LeftColumn from '../components/builder/left-column'
 import LoadModal from '../components/builder/modal/load'
@@ -8,12 +8,18 @@ import { builder, setRules } from '../state/builder'
 
 const BuilderPage = () => {
   const { isOpen, onOpen, onClose } = useDisclosure()
+  const toast = useToast()
   const builderState = builder.use()
   const [rule, setRule] = useState<string>('')
 
   const onSave = () => {
     if (rule === '') return
     setRules([...builderState.rules.filter(r => r !== rule), rule])
+    toast({
+      title: 'Rules saved',
+      status: 'success',
+      duration: 3000,
+    })
   }
 
   return (
