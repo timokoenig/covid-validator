8b9bff04ef51a5463a5ec1f24a70ddfb2aaaae93
Timo Koenig <timo@timokoenig.com>
2022-02-11 15:28:54 +0100

    Add placeholder component

---
+++ components/builder/placeholder.tsx
@@ -0,0 +1,12 @@
+import { Box, Text } from '@chakra-ui/react'
+import React from 'react'
+
+const Placeholder = () => {
+  return (
+    <Box p="10">
+      <Text textAlign="center">No Rules available</Text>
+    </Box>
+  )
+}
+
+export default Placeholder
--- components/builder/right-column.tsx
+++ components/builder/right-column.tsx
@@ -1,10 +1,34 @@
-import { Box, Text } from '@chakra-ui/react'
+import { AddIcon, DeleteIcon } from '@chakra-ui/icons'
+import { Box, Button, Flex, Text } from '@chakra-ui/react'
 import React from 'react'
+import Placeholder from './placeholder'
 
 const RightColumn = () => {
   return (
-    <Box flex="1" px="10" py="5">
-      <Text>Box 3</Text>
+    <Box flex="1" px="10" py="5" display="flex" flexDirection="column">
+      <Placeholder />
+      <Box mb="5">
+        <Box borderRadius="10" backgroundColor="gray.700" padding="5">
+          <Flex>
+            <Box flex="1" display="flex" flexDireaction="row" alignItems="center">
+              <Text fontWeight="semibold">V0.0.0.0</Text>
+            </Box>
+            <Button colorScheme="red">
+              <DeleteIcon width="3" height="3" />
+            </Button>
+          </Flex>
+        </Box>
+      </Box>
+      <Button
+        border="2px"
+        borderStyle="dashed"
+        borderColor="gray.700"
+        backgroundColor="transparent"
+        flex="0 1"
+        p="5"
+      >
+        <AddIcon width="3" height="3" />
+      </Button>
     </Box>
   )
 }
