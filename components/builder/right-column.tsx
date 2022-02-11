564a6c3ec6f2bcf7cef3a89517d4637e965f13b0
Timo Koenig <timo@timokoenig.com>
2022-02-11 15:07:36 +0100

    Create right column component for builder

---
+++ components/builder/right-column.tsx
@@ -0,0 +1,12 @@
+import { Box, Text } from '@chakra-ui/react'
+import React from 'react'
+
+const RightColumn = () => {
+  return (
+    <Box flex="1" px="10" py="5">
+      <Text>Box 3</Text>
+    </Box>
+  )
+}
+
+export default RightColumn
