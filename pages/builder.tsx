1d735a74c5cbae97aba6888c1c97bdf096e6ecf9
Timo Koenig <timo@timokoenig.com>
2022-02-11 15:35:44 +0100

    Add rule builder state to create rules

--- pages/builder.tsx
+++ pages/builder.tsx
@@ -1,16 +1,17 @@
 import { Flex } from '@chakra-ui/react'
-import React from 'react'
+import React, { useState } from 'react'
 import LeftColumn from '../components/builder/left-column'
 import RightColumn from '../components/builder/right-column'
 import PageMeta from '../components/page-meta'
 
 const BuilderPage = () => {
+  const [rules, setRules] = useState<string[]>([])
   return (
     <>
       <PageMeta allowIndex={false} />
       <Flex color="white" height="100vh">
         <LeftColumn />
-        <RightColumn />
+        <RightColumn rules={rules} onChange={setRules} />
       </Flex>
     </>
   )
