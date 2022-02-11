65303ed1eab7170998e7f00f5996871d4af209ea
Timo Koenig <timo@timokoenig.com>
2022-02-11 15:35:44 +0100

    Add rule builder state to create rules

--- components/builder/right-column.tsx
+++ components/builder/right-column.tsx
@@ -1,16 +1,31 @@
 import { AddIcon } from '@chakra-ui/icons'
-import { Box, Button } from '@chakra-ui/react'
+import { Box, Button, SimpleGrid } from '@chakra-ui/react'
 import React from 'react'
 import Placeholder from './placeholder'
 import Rule from './rule'
 
-const RightColumn = () => {
+type Props = {
+  rules: string[]
+  onChange: (rules: string[]) => void
+}
+
+const RightColumn = (props: Props) => {
+  const onAdd = () => {
+    props.onChange([...props.rules, '1'])
+  }
+
+  const onDelete = () => {
+    props.onChange([])
+  }
+
   return (
     <Box flex="1" px="10" py="5" display="flex" flexDirection="column">
-      <Placeholder />
-      <Box mb="5">
-        <Rule />
-      </Box>
+      {props.rules.length === 0 && <Placeholder />}
+      <SimpleGrid mb="5" spacing="5">
+        {props.rules.map(rule => (
+          <Rule key={rule} onDelete={onDelete} />
+        ))}
+      </SimpleGrid>
       <Button
         border="2px"
         borderStyle="dashed"
@@ -18,6 +33,7 @@ const RightColumn = () => {
         backgroundColor="transparent"
         flex="0 1"
         p="5"
+        onClick={onAdd}
       >
         <AddIcon width="3" height="3" />
       </Button>
--- components/builder/rule.tsx
+++ components/builder/rule.tsx
@@ -2,14 +2,18 @@ import { DeleteIcon } from '@chakra-ui/icons'
 import { Box, Button, Flex, Text } from '@chakra-ui/react'
 import React from 'react'
 
-const Rule = () => {
+type Props = {
+  onDelete: () => void
+}
+
+const Rule = (props: Props) => {
   return (
     <Box borderRadius="10" backgroundColor="gray.700" padding="5">
       <Flex>
         <Box flex="1" display="flex" flexDireaction="row" alignItems="center">
           <Text fontWeight="semibold">V0.0.0.0</Text>
         </Box>
-        <Button colorScheme="red">
+        <Button colorScheme="red" onClick={props.onDelete}>
           <DeleteIcon width="3" height="3" />
         </Button>
       </Flex>
