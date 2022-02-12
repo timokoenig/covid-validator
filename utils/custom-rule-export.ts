f402fbc0180c68eb095707359a927d086c3d001c
Timo Koenig <timo@timokoenig.com>
2022-02-12 02:17:05 +0100

    Make qrcode generation async

--- utils/custom-rule-export.ts
+++ utils/custom-rule-export.ts
@@ -2,7 +2,7 @@ import base45 from 'base45'
 import zlib from 'pako'
 import { CustomRule } from './certlogic'
 
-function customRuleExport(customRule: CustomRule): string {
+async function customRuleExport(customRule: CustomRule): Promise<string> {
   const compressedData = zlib.deflate(JSON.stringify(customRule))
   const encodedData = base45.encode(compressedData)
   return `CR:${encodedData}`
