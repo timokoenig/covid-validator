eb130a20ece3e2e306335b183b810eefdf912555
Timo Koenig <timo@timokoenig.com>
2022-02-20 14:48:55 +0100

    Remove builder stack from empty view

--- components/builder/right-column/empty.tsx
+++ components/builder/right-column/empty.tsx
@@ -1,30 +1,13 @@
 import { Box, Text } from '@chakra-ui/react'
-import React, { useState } from 'react'
+import React from 'react'
 import { useTranslation } from 'react-i18next'
-import { JSONObject } from '../../../utils/builder/types'
-import BuilderStack from '../stack'
 
 const EmptyComponent = () => {
-  // const [data, setData] = useState<JSONObject>({
-  //   if: [{ var: 'payload.0.1.2' }, { if: [{ var: 'payload.0.1.2' }, false, false] }, false],
-  // })
-  // const [data, setData] = useState<JSONObject>({
-  //   type: 'certificate-type',
-  //   if: [
-  //     { var: 'payload.v.0' },
-  //     {
-  //       and: [{ var: 'payload.0.1.2' }, { var: 'payload.0.1.2' }, true, false, 'hellp'],
-  //     },
-  //     false,
-  //   ],
-  // })
-  const [data, setData] = useState<JSONObject | null>(null)
   const { t } = useTranslation('common')
   return (
     <Box flex="1" px="10" py="5" display="flex" flexDirection="column" overflow="scroll">
       <Box p="10">
         <Text textAlign="center">{t('builder.placeholder')}</Text>
-        <BuilderStack data={data} onChange={setData} />
       </Box>
     </Box>
   )
