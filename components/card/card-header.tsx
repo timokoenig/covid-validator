aac56febc0137f82ddb8d7748b8e95ac894017dd
Moritz Veltrup <moritz.veltrup@gmail.com>
2022-02-20 23:17:39 +0100

    Add rule modal

--- components/card/card-header.tsx
+++ components/card/card-header.tsx
@@ -1,9 +1,25 @@
-import { Box, Text } from '@chakra-ui/react'
+import { Box, Button, ButtonProps, Text, useDisclosure } from '@chakra-ui/react'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { app } from '../../state/app'
 import countries from '../../utils/countries'
 import Flag from '../flag'
+import RulesModal from '../modal/rules'
+
+const CustButton = (props: ButtonProps) => (
+  <Button
+    {...props}
+    backgroundColor="inherit"
+    pl="5"
+    py="3"
+    h="auto"
+    borderRadius="0"
+    _hover={{ bgColor: 'blue.400' }}
+    _active={{ bgColor: 'blue.300' }}
+  >
+    {props.children}
+  </Button>
+)
 
 const CardHeader = () => {
   const { t } = useTranslation('country')
@@ -11,26 +27,32 @@ const CardHeader = () => {
   const allCountries = countries(t)
   const country = allCountries.find(item => item.code == appState.country) ?? allCountries[0]
   const state = country.states.find(item => item.code == appState.state) ?? country.states[0]
+  const { isOpen: isOpenRules, onOpen: onOpenRules, onClose: onCloseRules } = useDisclosure()
 
   return (
-    <Box display="flex" flexDirection="row" px="5" py="3" backgroundColor="blue.300">
-      <Box display="flex" alignItems="center">
-        <Flag country={country.code.toLowerCase()} size={25} />
+    <>
+      <Box display="flex" flexDirection="row" backgroundColor="blue.300">
+        <CustButton flex="1" onClick={onOpenRules}>
+          <Box display="flex" alignItems="center">
+            <Flag country={country.code.toLowerCase()} size={25} />
+          </Box>
+          <Box textAlign="center" flex="1" mx="6" color="white">
+            <Text fontSize="lg" wordBreak="break-word" whiteSpace="normal" fontWeight="bold">
+              {country.name}
+            </Text>
+            <Text fontSize="md" wordBreak="break-word" whiteSpace="normal" fontWeight="400">
+              {state.name}
+            </Text>
+          </Box>
+          <Box display="flex" alignItems="center">
+            <Text fontSize="lg" justifySelf="flex-end" fontWeight="bold" color="white">
+              {country.code === 'DE' ? appState.purpose : ''}
+            </Text>
+          </Box>
+        </CustButton>
       </Box>
-      <Box textAlign="center" flex="1" mx="6" color="white">
-        <Text fontSize="lg" wordBreak="break-word" whiteSpace="normal" fontWeight="bold">
-          {country.name}
-        </Text>
-        <Text fontSize="md" wordBreak="break-word" whiteSpace="normal" fontWeight="400">
-          {state.name}
-        </Text>
-      </Box>
-      <Box display="flex" alignItems="center">
-        <Text fontSize="lg" justifySelf="flex-end" fontWeight="bold" color="white">
-          {country.code === 'DE' ? appState.purpose : ''}
-        </Text>
-      </Box>
-    </Box>
+      <RulesModal isOpen={isOpenRules} onClose={onCloseRules} />
+    </>
   )
 }
 
