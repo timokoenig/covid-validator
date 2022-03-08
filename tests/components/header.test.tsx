9dde3498ecf3bb2752309f1a6109862ad4a29e56
Timo Koenig <timo@timokoenig.com>
2022-03-08 09:35:16 +0100

    Add ChakraProvider to Header snapshot test

--- tests/components/header.test.tsx
+++ tests/components/header.test.tsx
@@ -1,18 +1,37 @@
+import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
 import renderer from 'react-test-renderer'
 import Header from '../../components/header'
 
 it('renders correctly', () => {
-  const tree = renderer.create(<Header showMenu={true} version="1" />).toJSON()
+  const tree = renderer
+    .create(
+      <ChakraProvider>
+        <Header showMenu={true} version="1" />
+      </ChakraProvider>
+    )
+    .toJSON()
   expect(tree).toMatchSnapshot()
 })
 
 it('renders correctly without version', () => {
-  const tree = renderer.create(<Header showMenu={true} />).toJSON()
+  const tree = renderer
+    .create(
+      <ChakraProvider>
+        <Header showMenu={true} />
+      </ChakraProvider>
+    )
+    .toJSON()
   expect(tree).toMatchSnapshot()
 })
 
 it('renders correctly without menu', () => {
-  const tree = renderer.create(<Header showMenu={false} />).toJSON()
+  const tree = renderer
+    .create(
+      <ChakraProvider>
+        <Header showMenu={false} />
+      </ChakraProvider>
+    )
+    .toJSON()
   expect(tree).toMatchSnapshot()
 })
