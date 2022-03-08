75c16463f5794221e946a7317469dc2acb8a72d0
Timo Koenig <timo@timokoenig.com>
2022-03-08 10:15:45 +0100

    Set up local storage mock for jest

---
+++ tests/setup-tests.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+const localStorageMock = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  removeItem: jest.fn(),
+  clear: jest.fn(),
+}
+global.localStorage = localStorageMock
