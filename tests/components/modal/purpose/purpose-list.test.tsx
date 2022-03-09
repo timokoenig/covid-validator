895dc73a494318d1bcaf5b8f4c61736761158603
Timo Koenig <timo@timokoenig.com>
2022-03-09 19:01:21 +0100

    Add snapshot test for PurposeList component

--- tests/components/modal/purpose/purpose-list.test.tsx
+++ tests/components/modal/purpose/purpose-list.test.tsx
@@ -1,13 +1,15 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
 import renderer from 'react-test-renderer'
-import PurposeModal from '../../../../components/modal/purpose'
+import PurposeList from '../../../../components/modal/purpose/purpose-list'
+import { Purpose } from '../../../../utils/models'
 
 it('renders correctly', () => {
+  const purpose: Purpose = { title: 'foo', info: 'bar' }
   const tree = renderer
     .create(
       <ChakraProvider>
-        <PurposeModal isOpen={false} onClose={() => {}} />
+        <PurposeList items={[purpose]} selectedItem={purpose} onChange={() => {}} />
       </ChakraProvider>
     )
     .toJSON()
