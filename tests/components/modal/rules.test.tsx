0a620d4c440b4fb875075529e216bc3aa25b88cc
Timo Koenig <timo@timokoenig.com>
2022-03-09 18:53:36 +0100

    Add mock for Chakra PortalManager to fix modal snapshot tests

--- tests/components/modal/rules.test.tsx
+++ tests/components/modal/rules.test.tsx
@@ -1,8 +1,15 @@
+import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
 import renderer from 'react-test-renderer'
 import RulesModal from '../../../components/modal/rules'
 
 it('renders correctly', () => {
-  const tree = renderer.create(<RulesModal isOpen={false} onClose={() => {}} />).toJSON()
+  const tree = renderer
+    .create(
+      <ChakraProvider>
+        <RulesModal isOpen={true} onClose={() => {}} />
+      </ChakraProvider>
+    )
+    .toJSON()
   expect(tree).toMatchSnapshot()
 })
