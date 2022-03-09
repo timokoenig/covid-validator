1a392e02a42123f308bcf9a04892f4f9ee7810bb
Timo Koenig <timo@timokoenig.com>
2022-03-09 01:14:54 +0100

    Add snapshot test for InformatinoBlock component

--- tests/components/information/index.test.tsx
+++ tests/components/information/index.test.tsx
@@ -1,8 +1,15 @@
+import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
 import renderer from 'react-test-renderer'
 import Information from '../../../components/information'
 
 it('renders correctly', () => {
-  const tree = renderer.create(<Information />).toJSON()
+  const tree = renderer
+    .create(
+      <ChakraProvider>
+        <Information />
+      </ChakraProvider>
+    )
+    .toJSON()
   expect(tree).toMatchSnapshot()
 })
